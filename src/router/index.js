bc303d3c625d13451f4a92d851fd0341b033eb07
WOONAEEUN <158740755+WOONAEEUN@users.noreply.github.com>
2024-04-21 10:23:47 +0900

    app.vue & join.vue

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/Login',
-    name: 'Login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('@/views/Login.vue')
-  },
-  {
     path: '/Join',
     name: 'Join',
     // route level code-splitting
