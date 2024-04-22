7883d7303722bb1cd916607bd443da0260e241bc
2360314 <2360314@kbfg.com>
2024-04-22 17:05:28 +0900

    kakao login

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import store from '@/store/vuex'
 
 const app = createApp(App).use(router)
 app.use(vuetify)
+app.use(store)
 app.mount('#app')
+
+window.Kakao.init("a25065da4bb3c19c7bf0effe59e8ff0e");
