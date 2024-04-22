8e82332d3e3d14755eb52374e7b91de046c4f4bb
Hyzee2 <153643940+Hyzee2@users.noreply.github.com>
2024-04-22 17:26:40 +0900

    store

--- src/store/vuex.js
+++ src/store/vuex.js
@@ -4,17 +4,16 @@ import { createStore } from 'vuex'
 const store = createStore( {
     state: {
         user: {  // 새로운 상태 추가
-            id: null,
-            name: null
+            id: null
         }
     },
     mutations: {            // Java의 setter 개념 - 데이터 넘겨주기
         setUser(state, userInfo) {  // 파라미터 구조 분해 할당 사용
-            const { id, name } = userInfo;
+            const { id } = userInfo;
             console.log(userInfo);
             console.log("ID", id);
             state.user.id = id;
-            state.user.name = name;
+         
         },
        
             },
